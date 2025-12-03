79a303a0948129c0db94d5cf2d109b55aa14892e
Rachel <rachel.monero@gmail.com>
2025-12-02 22:27:12 -0500

    Update settings.py

--- now_we_talk/settings.py
+++ now_we_talk/settings.py
@@ -24,7 +24,7 @@ GOOGLE_APPLICATION_CREDENTIALS = os.path.join(BASE_DIR, 'service_key', 'nowwetal
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2$icri_xx6$wyu^&efv93wigu!m%)@+&##cl=3pjdqmjrp#1me'
+SECRET_KEY = 'Add_UR_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
